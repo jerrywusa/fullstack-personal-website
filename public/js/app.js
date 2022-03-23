fca710dfe12220dbd257d31748b3b7869ff4688a
jerrywusa <mrjerryowu@gmail.com>
2022-03-22 20:31:40 -0700

    removed user select from AboutPage

--- public/js/app.js
+++ public/js/app.js
@@ -12109,7 +12109,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ".OWwnopXQJ0RjUdtmp7CKNw\\=\\= {\n  width: 100vw;\n  height: 100vh;\n  overflow-x: hidden;\n  overflow-y: overlay;\n  z-index: 1;\n}\n\n.sKidB26WbOGth0A0T51kdQ\\=\\= {\n  font-size: 80pt;\n  margin-top: 8vh;\n  margin-bottom: 8vh;\n  margin-left: 6vw;\n  color: #ebdbb2;\n  z-index: 1;\n}\n\n.zcwLf03mhA3s1EUjPNNoMw\\=\\= {\n  display: flex;\n  flex-direction: row;\n  flex-wrap: wrap;\n  align-items: center;\n  justify-content: space-evenly;\n}", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, ".OWwnopXQJ0RjUdtmp7CKNw\\=\\= {\n  width: 100vw;\n  height: 100vh;\n  overflow-x: hidden;\n  overflow-y: overlay;\n  z-index: 1;\n  -webkit-user-select: none;\n     -moz-user-select: none;\n      -ms-user-select: none;\n          user-select: none;\n}\n\n.sKidB26WbOGth0A0T51kdQ\\=\\= {\n  font-size: 80pt;\n  margin-top: 8vh;\n  margin-bottom: 8vh;\n  margin-left: 6vw;\n  color: #ebdbb2;\n  z-index: 1;\n}\n\n.zcwLf03mhA3s1EUjPNNoMw\\=\\= {\n  display: flex;\n  flex-direction: row;\n  flex-wrap: wrap;\n  align-items: center;\n  justify-content: space-evenly;\n}", ""]);
 // Exports
 ___CSS_LOADER_EXPORT___.locals = {
 	"container": "OWwnopXQJ0RjUdtmp7CKNw==",
